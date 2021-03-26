ad2b8d67bc3e6130d04eb5eb4bf19f403e417ab9
Maxra32 <rankmaximilian@gmail.com>
2021-03-26 15:18:30 +0100

    Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
 import '../styles/globals.scss'
 import 'carbon-components/scss/globals/scss/styles.scss';
+import styles from '../styles/Footer.module.scss'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+<Component {...pageProps} />
+<footer className={styles.footer}>
+  <a
+    href="https://www.rank.health/imprint"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    With love 🖤{' '}
+    <img src="/Logo_full_black no background.png" alt="rank.health Logo" className={styles.logo} />
+  </a>
+</footer>
+</div>
+
 }
+
+
 
 export default MyApp
