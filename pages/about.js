5052b2c5aa0150029f369af9e12babb6f09052ce
Maxra32 <rankmaximilian@gmail.com>
2021-03-26 12:49:52 +0100

    added carbon components

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import styles from '../styles/about.module.css'
+import { Button } from 'carbon-components-react';
+import 'carbon-components/scss/globals/scss/styles.scss';
+
+function About() {
+  return <div className={styles.container}>
+  <Button>
+    DEMO TITLE BOSS ASS WITCHES
+  </Button>
+</div>
+}
+
+export default About
