6c5e81d6727cefb5ea060d5a07aa753468ee29d7
Maxra32 <rankmaximilian@gmail.com>
2021-06-07 21:59:22 +0200

    fix: 404 msg

--- pages/404.js
+++ pages/404.js
@@ -10,7 +10,7 @@ function Error({ statusCode }) {
       <img src="/404.gif" alt="Laika in Space" className={styles.img} />
         {statusCode
           ? `Seems like you're lost in ${statusCode}`
-          : 'Seems like you are lost in'}
+          : 'Seems like you are lost in 404'}
       </p>
 
       <div className={styles.space}>
