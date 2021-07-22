2bd817830fd9a0974c34955fa02fe6fc40dcec35
Maxra32 <rankmaximilian@gmail.com>
2021-07-22 20:44:07 +0200

    chores: cleaned up some texts

--- pages/about.js
+++ pages/about.js
@@ -12,21 +12,20 @@ function About() {
 
     <div className={styles.container}>
       <p className={styles.description}>
-      Not long ago I started my career as PO in a health tech startup. I knew I didn't want to spend my life working for
-      companies which build products that just waste people's precious life time - like mobile games.
+      Not so long ago I started my career as PO in health tech. I knew I didn't want to spend my time working for
+      companies which build products that just waste people's time - like mobile games.
       <br />
-      Instead, I wanted to help fantastic teams to build great products which have a positive impact on people's lives🐱‍💻.
+      Instead, I wanted to help fantastic teams to build great products which leave a positive impact on people's lives🐱‍💻.
       <br /><br />
       </p>
       <p className={styles.description}>
       Turns out, since then I spent most of my time on regulatory issues - either as product guy or regulatory consultant for DiGAs.
       </p>
       <p className={styles.description}>
-      And that's a problem for anyone who wants to f(o)und a health tech startup: Kicking-off a business in this space is rough. Why? Not only because it's
-      hard to find a working business model. But because suddenly so many regulatory requirements apply to you.
+      And that's a problem for anyone who wants to f(o)und a health tech startup: Suddenly, so many regulatory requirements apply to you.
       </p>
       <p className={styles.description}>
-      And the toughest part isn't even to implement these regulatory requriements. But just to understand which of them apply in the first place and
+      And the toughest part isn't even to implement these requriements. But just to understand which of them apply in the first place and
       what stuff like "state of the art transport encryption" (wrapped in complicated lawmaker's language nobody understands) exactly means for implementation (hint: simply enforcing TLS 1.2 or higher is not enough).
       <br /><br />
       </p>
@@ -37,13 +36,13 @@ function About() {
       </p>
       <p className={styles.description}>
       Now, this would not be an issue if all the legal requirements would help making your product and processes more safe.
-      However the worst part is: A lot of the required documentation and processes is just excessive regulatory frenzy and does not increase you product's security by a
-      bit (literally 😄). For example, I've never seen any "Datenschutzfolgenabschätzung" that actually led to a new feature being developed, which actually helps making your user's health data more safe.
+      However, the worst part is: A lot of the required documentation and processes is just excessive regulatory frenzy and does not increase you product's security by a
+      bit (literally 😄). For example, I've never seen any "Datenschutzfolgenabschätzung" that actually led to a new feature being developed, which actually helps making your user's health data more secure.
       <br /><br />
       </p>
       <p className={styles.description}>
       So, to make life a little easier for you I decided to publish all of my practical experience as DiGA consultant
-      on this website - for free (yes, free lunches are possible I don't care what some Economists say about this). You're welcome. Hopefully it helps you
+      on this website - for free (yes, free lunches are possible, I don't care what some Economists say about this). You're welcome. Hopefully it helps you
       spend more time on what actually matters: Your users.
       <br></br>
       <br></br>
--- pages/introduction.js
+++ pages/introduction.js
@@ -12,10 +12,9 @@ function About() {
 
     <div className={styles.container}>
       <p className={styles.description}>
-        Through DVG the German lawmaker gives you a chance to make your product prescribable by doctors and be reimbursed
-        by public health insurances. That's great news 🎉. For the first time many health tech startups got the chance to build their business
-        around a working business model. Because let's be honest: Nobody pays for health apps and selective contracts with health insurances rarely ever
-        return enough revenue.
+        With DVG the German lawmaker provides you an opportunity to make your product prescribable by doctors and be reimbursed
+        by public health insurances. That's great news 🎉. For the first time many health tech startups got the chance to be build around a working business model.
+        Because, let's face it: Nobody pays for health apps and selective contracts with health insurances rarely ever pay off.
       </p>
       <p className={styles.description}>
         Aye, so much for the good part. Because becoming a digital health application (DiGA) also comes at a price. High regulatory demands from many institutions will
@@ -31,13 +30,13 @@ function About() {
       <p className={styles.description}>
         <br /><br />
         Yeh, don't worry. There are more laws which apply to you and more regulation you have to follow. But these four require the largest chunk of work.
-        If you decide you want your product to become a DiGA listed with BfArM (the authority which checks whether you actually fulfill all DVG requirements)
+        If you decide you want to become a DiGA listed with BfArM (the authority which checks whether you actually fulfill all DVG requirements)
         then make sure your product is able to do at least this:
         <br /><br />
-        ► Your product has measurable positive medical effects on people with a certain medical condition (or at least you believe it has) <br />
+        ► Your product has measurable positive medical impact on people with a certain medical condition (or at least you believe it has) <br />
         ► The risk entailed by using your product does not cause significant physical or psychological harm (you are of low risk-class, I or IIa according to MDR) <br />
         Basically this means you are MDR compliant and carry a CE certification, which TÜV (or any other institution like them) will verify<br />
-        ► Your product's effect does NOT rely on added services, like interaction with therapists, coaches, doctors etc. The effect must come from your app itself.<br />
+        ► Your product's effect does NOT rely on added services, like interaction with therapists, coaches, doctors etc. The effect must stem from the app itself.<br />
         There is a grey area, which I describe in <a
           href="https://www.rank.health/faq"
           target="_blank"
@@ -45,7 +44,7 @@ function About() {
         >→the FAQ</a> because it's frequently asked<br /><br />
       </p>
       <p className={styles.description}>
-        Got all this ready? Still want to become DiGA? Cool, then just follow this step-by-step guide and you'll be good to go.
+        Got all this? Still want to become DiGA? Cool, then just follow this step-by-step guide and you'll be good to go.
         <br /><br /><br />
       </p>
     </div>
